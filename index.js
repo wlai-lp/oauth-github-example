009c9ee9dbb060625fa02570b5198c7eee05e7c0
Wei Lai <wlai@liveperson.com>
2022-11-07 13:04:32 -0500

    auth0 code in callback url

--- index.js
+++ index.js
@@ -13,27 +13,13 @@ app.get('/', (req, res) => {
 
 app.get('/auth', (req, res) => {
   res.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+    `https://dev-ebsf4fc7.us.auth0.com/oauth/device/code?client_id=${process.env.GITHUB_CLIENT_ID}`,
   );
 });
 
-app.get('/oauth-callback', ({ query: { code } }, res) => {
-  const body = {
-    client_id: process.env.GITHUB_CLIENT_ID,
-    client_secret: process.env.GITHUB_SECRET,
-    code,
-  };
-  const opts = { headers: { accept: 'application/json' } };
-  axios
-    .post('https://github.com/login/oauth/access_token', body, opts)
-    .then((_res) => _res.data.access_token)
-    .then((token) => {
-      // eslint-disable-next-line no-console
-      console.log('My token:', token);
-
-      res.redirect(`/?token=${token}`);
-    })
-    .catch((err) => res.status(500).json({ err: err.message }));
+app.get('/callback', ({ query: { code } }, res) => {
+  console.log('My code:', code);
+  res.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
 app.listen(3000);
